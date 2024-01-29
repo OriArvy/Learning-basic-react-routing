2959f0559f176176762f24fb3bded65d1ef42865
Rokas <rokas.vaitkevicius1@gmail.com>
2024-01-30 01:59:27 +0200

    Added useNavigate for demo purposes + isActive functionality on navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate('/products')
+  }
+
   return (
     <div>
       <h1>My home page</h1>
       <p>
         Go to the list of <Link to="/products">products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </div>)
 }
