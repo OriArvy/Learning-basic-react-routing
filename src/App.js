1df60c518a37131909e71c63903e2d3b3bafc693
Rokas <rokas.vaitkevicius1@gmail.com>
2024-01-30 01:49:24 +0200

    Added error page for routes that dont exist, plus some clean up/refactoring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
-import MainNavigation from './components/MainNavigation';
 import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
 
 
 const router = createBrowserRouter([
-  { path: '/', element: <RootLayout />, children: [
-    { path: '/',  element: <HomePage /> },
-    { path: '/products', element: <ProductsPage /> }
+  { path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/',  element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> }
   ]}
 
 ]);
